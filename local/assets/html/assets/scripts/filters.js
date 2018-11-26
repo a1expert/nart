abc86a2d2feee4b3ea541b91d6e177a2e8a9c0c8
Konstantin <zkkorion@gmail.com>
2018-11-26 17:14:54 +0500

    replace base html

--- local/assets/html/assets/scripts/filters.js
+++ local/assets/html/assets/scripts/filters.js
@@ -3,8 +3,50 @@
 (function (){
 	const filters = document.querySelector('.filters__block');
 
-	const sortingCardsByPrices = function (array) {
-		let prices = array.slice().sort(function (a, b) {
+	const filter = {
+		seats: 'any',
+		type: 'any',
+		price: 'any'
+	};
+
+	const seatsFilter = (seats, comparedSeats) => {
+		if (comparedSeats === 'any') {
+			return true;
+		}
+		return seats === comparedSeats;
+	};
+
+	const typeFilter = (type, comparedType) => {
+		if (comparedType === 'any') {
+			return true;
+		}
+		return type === comparedType;
+	};
+
+	const applyFilters = arr => {
+		return arr.filter(it => {
+			return seatsFilter(it.seats, filter.seats) && typeFilter(it.type, filter.type);
+		});
+	};
+
+	const addFilter = (type, value) => {
+		filter[type] = value;
+	};
+
+	const activateFilterBtn = button => {
+		const siblingsFilterButtons = button.parentNode.children;
+		[].forEach.call(siblingsFilterButtons, function (it) {
+			it.classList.remove('filters__control_active');
+		});
+		button.classList.add('filters__control_active');
+	};
+
+	const deactivateFilterBtn = button => {
+		button.classList.remove('filters__control_active');
+	};
+
+	const sortByPrice = arr => {
+		let prices = arr.slice().sort(function (a, b) {
 			if (parseInt(a.price, 10) > parseInt(b.price, 10)) {
 				return 1;
 			}
@@ -16,61 +58,42 @@
 		return prices;
 	};
 
-	const toggleActiveFilterBtn = function (button) {
-		const allFilterButtons = button.parentNode.children;
-		[].forEach.call(allFilterButtons, function (it) {
-			it.classList.remove('filters__control_active');
-		});
-		button.classList.add('filters__control_active');
-	};
-
-	const rerenderCards = function (cardsArray) {
-		rentGrid.innerHTML = '';
-		renderCards(cardsArray);
-	};
-
-	const applyFilterByPrice = function (button) {
-		switch (button.id) {
+	const checkSorting = arr => {
+		switch (filter.price) {
+			case 'any':
+				return arr;
 			case 'priceUp':
-				debounce(function () {
-					rerenderCards(sortingCardsByPrices(window.cards));
-				});
-				break;
+				return sortByPrice(arr);
 			case 'priceDown':
-				debounce(function () {
-					rerenderCards(sortingCardsByPrices(window.cards).reverse());
-				});
-				break;
+				return sortByPrice(arr).reverse();
 		}
 	};
 
-	const disapplyFilterByPrice = function (button) {
-		button.classList.remove('filters__control_active');
-		debounce(function () {
-			rerenderCards(window.cards);
-		});
-	};
-
-	const filtersOnClick = function (evt) {
+	const filtersOnClick = evt => {
 		const target = evt.target;
-		switch (target.dataset.filteredby) {
-			case 'seats':
-				console.log('фильтрация по местам');
-				break;
-			case 'type':
-				console.log('фильтрация по типу');
-				break;
-			case 'price':
-				console.log('фильлтрация по цене');
-				break;
+		const filterBy = target.dataset.filterby;
+		const value = target.dataset.value;
+		let filteredCards = [];
+		let sortedCards = [];
+		if (!target.classList.contains('filters__control_active') && target.closest('button')) {
+			activateFilterBtn(target);
+			addFilter(filterBy, value);
+			filteredCards = applyFilters(window.cards);
+			sortedCards = checkSorting(filteredCards);
+			window.debounce(function () {
+				window.renderCards(sortedCards);
+			});
+		}else {
+			deactivateFilterBtn(target);
+			addFilter(filterBy, 'any');
+			filteredCards = applyFilters(window.cards);
+			window.debounce(function () {
+				window.renderCards(filteredCards);
+			});
 		}
-		// if (target === target.closest('button') && (!target.classList.contains('filters__control_active'))) {
-		// 	toggleActiveFilterBtn(target);
-		// 	applyFilterByPrice(target);
-		// }else if (target === target.closest('button')) {
-		// 	disapplyFilterByPrice(target);
-		// }
-
 	};
-	filters.addEventListener('click', filtersOnClick);
+
+	if (filters) {
+		filters.addEventListener('click', filtersOnClick);
+	}
 }());
