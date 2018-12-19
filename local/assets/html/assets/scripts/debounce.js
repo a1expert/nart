404a5d4333aecc267401367a27b1a3cda754d857
m1roku <m1roku@mail.ru>
2018-12-19 11:54:33 +0500

    new html/css

--- local/assets/html/assets/scripts/debounce.js
+++ local/assets/html/assets/scripts/debounce.js
@@ -1,11 +1,5 @@
 'use strict';
 
 (function (){
-	let lastTimeout;
-	window.debounce = function (func) {
-		if (lastTimeout) {
-			clearTimeout(lastTimeout);
-		}
-		lastTimeout = setTimeout(func, 500);
-	};
+
 }());
